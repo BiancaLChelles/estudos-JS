93f2ca1163e0969b508f86ff9e702b4b1daee722
Bianca Lourenço Chelles <bianca.l.o@outlook.com>
2026-02-01 10:59:03 -0300

    Create exercicio-status-bateria.js
    Exercício de Lógica: e Js; Diagnóstico de Saúde de Bateria 
    
    Este script foi desenvolvido como parte dos meus estudos de JavaScript para praticar o uso de funções, condicionais (if/else) e escopo de variáveis.
    
    A lógica foi inspirada em investigações reais de hardware que realizo no meu Lenovo Ideapad S145, onde busco entender a relação entre drivers, BIOS e o desempenho dos componentes. 
    
    O que este código faz:
    
    Recebe a capacidade de fábrica (Design) e a capacidade atual da bateria. 
    
    Calcula a porcentagem de desgaste.
    
    Retorna um diagnóstico pedagógico sobre a necessidade de calibração ou substituição.

---
+++ exercicio-status-bateria.js
@@ -0,0 +1,55 @@
+
+// nessa parte do código o usuario não deve mexer
+// dessa vez a parte do usuario interagir est no final pois as funcões precisam ser declaradas antes de serem chamadas
+// traduzindo, nesse caso tenho que falar pra maquina o que fazer com os dados, depois informo os dados
+// então nesse caso a interação do usuario com o codigo será no "rodapé"
+
+
+function porcentagemDeDesgaste ( capacidadeAtual, capacidadeDesign){ // aqui crio a função e dou á nome as variaveis necessarias pra ela funcionar
+
+let desgaste = 100 - ( capacidadeAtual / capacidadeDesign * 100); // aqui digo o que essa função deve fazer com essas variaveis
+
+let resultado = ' ' // crio a variavel de retorno de resultado, ela funciona como uma gaveta separada pra guardar algo que ainda não tenho
+
+if (desgaste < 20) { // inicio as analises para classificar o resultado e declarar minha resposta
+resultado = 'Bateria Saudável'
+} else if ( desgaste >= 20 && desgaste <= 50 ) {
+resultado = 'Sinais de desgaste. Considere calibrar'
+} else if ( desgaste > 50) {
+resultado = 'Substituição recomendada'
+} else {
+resultado ='Status da bateria não identificado.'
+}
+
+return resultado; // aqui grito a resposta pra fora da função
+}
+
+
+
+// aqui o usuário mexe ----
+
+
+let statusBateria = porcentagemDeDesgaste (capacidadeAtual, capacidadeDesign) 
+
+console.log (statusBateria) 
+
+// aqui nós  chamamos a função e preenchemos os campos 'capacidadeAtual' e 'capacidadeDesign'
+// o usuario pode modificar AS VARIAVEIS de acordo com o caso real, executar o código e aguardar que o retorno será exibido no console
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+assim ta certo? (eu não vou dar valores pras variaveis da função quem vai fazer isso é o usuario, só quero saber se está funcional e correto
