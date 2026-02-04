8d1f57e132d7e2d13ab5e61697ad1fe81d4db908
Bianca Lourenço Chelles <bianca.l.o@outlook.com>
2026-02-04 09:35:00 -0300

    Update exercicio-classificador-clima.js

--- exercicio-classificador-clima.js
+++ exercicio-classificador-clima.js
@@ -1,5 +1,5 @@
 // A variável T representa a temperatura (aqui o usuário define o valor)
-let T = 22; 
+let T = ' '; 
 
 
 
@@ -17,8 +17,8 @@ else if (T >= 18 && T <= 25) {
 else if (T > 25) {
     console.log('Clima de deserto!');
 } 
-// Caso o valor de T não seja um número ou não tenha sido definido
-// estamos previnindo que um erro de uso cause um erro no sistema
+// Caso o valor de T não seja um número ou não tenha sido definido já temos também uma resposta
+// Aqui estamos previnindo que um erro de uso cause um erro no sistema
 
 else {
     console.log('Clima não detectável');
