721a810095b27609efd4098c0c0a37d5d9a2c725
Bianca Lourenço Chelles <bianca.l.o@outlook.com>
2026-02-05 12:02:02 -0300

    Update exercicio-look-do-dia.js

--- exercicio-look-do-dia.js
+++ exercicio-look-do-dia.js
@@ -1,11 +1,11 @@
 
-// declaração das variaveis
+// Aqui criamos e declaramos as variaveis
 
 let camiseta1 = "branca";
 let short1 = "verde";
 let camiseta2 = "preta";
 let short2 = "verde";
 
-// saida de texto no sistema
+// Aqui temos a saida de texto no sistema, preenchendo as lacunas com os valores correspondentes aos das variaveis chamadas
 
 console.log ( " A Flávia está usando uma blusa " + camiseta1 + " e um shorts " + short1 + " \n Já a Renata, esta usando uma blusa " + camiseta2 + " e um shorts " + short2 + " \n ESSE È O LOOK DE HOJE!!!")
